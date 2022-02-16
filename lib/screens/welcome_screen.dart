0910336e76b7d8a1870c5e84044b48626fb4f8de
Jerry Agbesi <jerryeagbesi@gmail.com>
2022-02-15 23:16:15 -0800

    completed welcome Screen

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -33,29 +33,46 @@ class WelcomeScreen extends StatelessWidget {
               const SizedBox(
                 height: 20,
               ),
-              GestureDetector(
-                child: Container(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 40, vertical: 16),
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFFFFFFF),
-                    borderRadius: BorderRadius.circular(30),
-                    boxShadow: [
-                      BoxShadow(
-                        offset: Offset(0, 15),
-                        blurRadius: 30,
-                        color: Color(0xFF666666).withOpacity(0.11),
-                      ),
-                    ],
-                  ),
-                  child: const Text(
-                    'Start Reading',
-                    style: TextStyle(fontWeight: FontWeight.w800, fontSize: 16),
-                  ),
-                ),
-              ),
+              RoundButton(label: 'Start Reading',function: (){},),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class RoundButton extends StatelessWidget {
+  final String label;
+  final VoidCallback function;
+  final double fontsize;
+  const RoundButton({
+    required this.label,
+    required this.function,
+    this.fontsize = 16,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: function,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 16),
+        decoration: BoxDecoration(
+          color: const Color(0xFFFFFFFF),
+          borderRadius: BorderRadius.circular(30),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(0, 15),
+              blurRadius: 30,
+              color: Color(0xFF666666).withOpacity(0.11),
+            ),
+          ],
+        ),
+        child:  Text(
+          label,
+          style: TextStyle(fontWeight: FontWeight.w800, fontSize: fontsize),
         ),
       ),
     );
