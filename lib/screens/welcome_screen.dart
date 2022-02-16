f29406c06041c06ed4838e5590d212e427fc12b4
Jerry Agbesi <jerryeagbesi@gmail.com>
2022-02-15 23:23:17 -0800

    added barrel files and homescreen

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ebook_app/constants.dart';
+import 'package:ebook_app/widgets/widgets.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
@@ -33,46 +34,12 @@ class WelcomeScreen extends StatelessWidget {
               const SizedBox(
                 height: 20,
               ),
-              RoundButton(label: 'Start Reading',function: (){},),
+              RoundButton(
+                label: 'Start Reading',
+                function: () {},
+              ),
             ],
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class RoundButton extends StatelessWidget {
-  final String label;
-  final VoidCallback function;
-  final double fontsize;
-  const RoundButton({
-    required this.label,
-    required this.function,
-    this.fontsize = 16,
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: function,
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 16),
-        decoration: BoxDecoration(
-          color: const Color(0xFFFFFFFF),
-          borderRadius: BorderRadius.circular(30),
-          boxShadow: [
-            BoxShadow(
-              offset: Offset(0, 15),
-              blurRadius: 30,
-              color: Color(0xFF666666).withOpacity(0.11),
-            ),
-          ],
-        ),
-        child:  Text(
-          label,
-          style: TextStyle(fontWeight: FontWeight.w800, fontSize: fontsize),
         ),
       ),
     );
